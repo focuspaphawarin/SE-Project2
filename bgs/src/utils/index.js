52bd92dcde9da84c43ed17f934350d4d8005f53a
4 <paphawarin.focus@gmail.com>
2024-03-16 01:14:36 +0700

    front sign up

---
+++ bgs/src/utils/index.js
@@ -0,0 +1,72 @@
+export const registrationFormControls = [
+    {
+      id: "username",
+      type: "text",
+      placeholder: "Enter your username",
+      label: "UserName",
+      componentType: "input",
+    },
+    {
+      id: "email",
+      type: "email",
+      placeholder: "Enter your email",
+      label: "Email",
+      componentType: "input",
+    },
+    {
+      id: "password",
+      type: "password",
+      placeholder: "Enter your password",
+      label: "Password",
+      componentType: "input",
+    },
+    {
+        id: "phone_number",
+        type: "phone_number",
+        placeholder: "Enter your phone number",
+        label: "Phone Number",
+        componentType: "input",
+    },
+    {
+        id: "address",
+        type: "address",
+        placeholder: "Enter your address",
+        label: "Address",
+        componentType: "input",
+    },
+    {
+      id: "role",
+      type: "",
+      placeholder: "",
+      label: "Role",
+      componentType: "select",
+      options: [
+        {
+          id: "shop",
+          label: "Shop",
+        },
+        {
+          id: "customer",
+          label: "Customer",
+        },
+      ],
+    },
+  ];
+  
+  export const loginFormControls = [
+    {
+      id: "email",
+      type: "email",
+      placeholder: "Enter your email",
+      label: "Email",
+      componentType: "input",
+    },
+    {
+      id: "password",
+      type: "password",
+      placeholder: "Enter your password",
+      label: "Password",
+      componentType: "input",
+    },
+  ];
+  
